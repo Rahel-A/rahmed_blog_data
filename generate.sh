85a5a1b02065d5f5f6ea328c21df93f6459d764a
Rahel A <rahel@tuta.io>
2024-01-01 13:45:10 +0200

    add docker image bringup and website deployment

--- generate.sh
+++ generate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker-compose up -d
 HOST="localhost:8000"
 mkdir out
 curl $HOST/c4plantuml/svg --data-binary '@diagrams/deployment.puml' -o out/deployment.svg
@@ -6,4 +7,6 @@ curl $HOST/c4plantuml/png --data-binary '@diagrams/deployment.puml' -o out/deplo
 curl $HOST/c4plantuml/svg --data-binary '@diagrams/deployment-pi.puml' -o out/deployment-pi.svg
 curl $HOST/c4plantuml/svg --data-binary '@diagrams/deployment-truenas.puml' -o out/deployment-truenas.svg
 curl $HOST/mermaid/svg --data-binary '@diagrams/interview.md' -o out/interview.svg
+mv out ../../static/diagrams
+docker-compose down
 
